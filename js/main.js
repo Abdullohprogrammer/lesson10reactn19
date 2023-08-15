e18d2d4ca0a9be2e71ef633d91f4b750099f29e3
abdulloh <abdullohprogrammer@gmail.com>
2023-08-15 16:19:21 +0500

    qwerty

---
+++ js/main.js
@@ -0,0 +1,135 @@
+
+//     1-misol     //
+
+
+// var x = 5;
+
+// if(x > 0) {
+//    console.log('musbat')
+// }else console.log('manfit')
+
+
+
+//      2-misol     //
+
+
+// var x = 5;
+
+// if(x % 2 == 0) {
+//    console.log('juft son')
+// }else{
+//    console.log('toq son');
+// }
+
+
+
+
+//     3-misol    //
+
+
+// var x = 6;
+
+// if(x % 3 == 0) {
+//    console.log('juft son');
+// }else{
+//    console.log('toq son');
+// }
+
+
+
+//     4-misol     //
+
+
+// var x = 6;
+
+// var y = 7;
+
+// if(x > 0 && y > 0) {
+//    console.log('true');
+// }else('false')
+
+
+
+
+//     5-misol     //
+
+
+// var a = 8;
+
+// var b = 6;
+
+// var c = 4;
+
+// if( a > b && b > c) {
+//    console.log('true');
+// }else(console.log('false');)
+
+
+
+//     7-misol    //
+
+
+// var a = 12;
+
+// var b = 12;
+
+// if(a % 3 == 0 && b % 3 == 0) {
+//    console.log('true');
+// }else(console.log('false'))
+
+
+//      8-misol     //
+
+
+// var a = 9;
+
+// var b = 11;
+
+// var c = 13;
+
+// var d = 15;
+
+// if ( a > 0 && b > 0 && c > 0 && d > 0) {
+//   console.log('Sonlarni hammasi musbat');
+// }else {
+//   console.log('Sonlarni hammasi musbat emas');
+// }
+
+
+
+//      9-misol      //
+
+
+// var a = 8;
+
+// var b = 6;
+
+// var c = -4;
+
+// if(a > 0 && b > 0 && c < 0) {
+//    console.log('true');
+// }else(console.log('false'))
+
+
+
+//     10-misol     //
+
+
+// var haftakunlari = [
+//     '1-DUSHANBA',
+//     '2-SESHANBA',
+//     '3-CHORSHANBA',
+//     '4-PAYSHANBA',
+//     '5-JUMA',
+//     '6-SHANBA',
+//     '7-YAKSHANBA',
+// ]
+
+
+//     11-misol     //
+
+// var num90 = ['BELLINE']
+
+// var num99 = ['UZMOBILE']
+
+// var num93 = ['UCELL']
